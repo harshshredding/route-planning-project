b320935b9a902c843e75363e5a782509ea85cc18
Harsh Verma <harshshredding@gmail.com>
2022-12-04 16:44:42 -0500

    race condition constructor

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -10,8 +10,8 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
     // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
-    RouteModel::Node starting_node = this->m_Model.FindClosestNode(start_x, start_y);
-    RouteModel::Node ending_node = this->m_Model.FindClosestNode(end_x, end_y);
+    RouteModel::Node& starting_node = this->m_Model.FindClosestNode(start_x, start_y);
+    RouteModel::Node& ending_node = this->m_Model.FindClosestNode(end_x, end_y);
     this->start_node = &starting_node;
     this->end_node = &ending_node;
 }
